b371597e53fc72cbea62d0967712b202c303aaf5
Tomasz Stachowiak <tomek@tjstachowiak@gmail.com>
2019-02-01 01:58:20 +0100

    next test added

--- start/start.test.js
+++ start/start.test.js
@@ -3,3 +3,8 @@ const testFunction = require('./code');
 test('check if greets the world properly', () => {
   expect(testFunction()).toBe("Hello World");
 });
+
+
+test('check if greets the Tomek properly', () => {
+  expect(testFunction("Tomek")).toBe("Tomek World");
+});
