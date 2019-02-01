4d18e16b99d0403169f680ae5361fdce01f94e55
Tomasz Stachowiak <tjstachowiak@gmail.com>
2019-02-01 03:19:12 +0100

    random name test

--- start/start.test.js
+++ start/start.test.js
@@ -8,3 +8,10 @@ test('check if greets the world properly', () => {
 test('check if greets the Tomek properly', () => {
   expect(testFunction("Tomek")).toBe("Hello Tomek");
 });
+
+
+test('check if greets the random bullshit properly', () => {
+  const randomName = Math.random().toString(36).substring(7);
+  expect(testFunction(randomName)).toBe(`Hello ${randomName}`);
+});
+
