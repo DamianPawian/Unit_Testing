a53732c46261d70dd78cd8ab59998208285c9291
Damian <damian.b.134@gmail.com>
2019-02-12 20:04:51 +0100

    all test passed

--- start/code.js
+++ start/code.js
@@ -1,17 +1,10 @@
 
-const startFunction = (val) => {
-
-  if (val === "Tomek") {
-    return "Hello Tomek";
-  } else if (val !== "Tomek" ){
-    return "Hello " + randomName;
-  } else if (val !== "Tomek" && val !== randomName ){
-    return "Hello World";
-  }
+const startFunction = (val = "World") => {
+return "Hello " + val;
 };
 
 
-const randomName = Math.random().toString(36).substring(7);
+
 
 
 
